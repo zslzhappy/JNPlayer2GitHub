1087eaf8a914f7e29bc805192721059c1359f9ba
John <ztgravity@163.com>
2016-10-12 14:05:31 +0800

    init project

---
+++ JNPlayerKit/JNPlayerKit/JNPlayerKit.h
@@ -0,0 +1,19 @@
+//
+//  JNPlayerKit.h
+//  JNPlayerKit
+//
+//  Created by mac on 16/10/12.
+//  Copyright © 2016年 Magugi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JNPlayerKit.
+FOUNDATION_EXPORT double JNPlayerKitVersionNumber;
+
+//! Project version string for JNPlayerKit.
+FOUNDATION_EXPORT const unsigned char JNPlayerKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JNPlayerKit/PublicHeader.h>
+
+
---
+++ JNPlayerKit/JNPlayerKit/JNPlayerView.swift
@@ -0,0 +1,30 @@
+//
+//  JNPlayerView.swift
+//  JNPlayerKit
+//
+//  Created by mac on 16/10/12.
+//  Copyright © 2016年 Magugi. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+public class JNPlayerView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+    
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    public required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+}
